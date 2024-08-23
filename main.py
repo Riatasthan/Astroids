41664f98ea7ddddd166691c0d3f6fbc06732d9f0
Riatasthan <153391887+Riatasthan@users.noreply.github.com>
2024-08-23 17:15:55 +0100

    Initialising and screen size

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+# Importing module
 import pygame
 
+# Initalising the module
+pygame.init()
+
+# Imporitng constants from constants file
+from constants import *
+
+# Setting screen size 
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+# Main loop
 def main():
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+
 if __name__ == "__main__":
     main()
