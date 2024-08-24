4949fec1bd23b1f9c98a7f773336ab5569b09b0b
Riatasthan <153391887+Riatasthan@users.noreply.github.com>
2024-08-24 08:28:16 +0100

    Break commit

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
     pygame.init()
     # Setting screen size 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    # Clock and delta time
+    clock = pygame.time.Clock()
+    dt = 0
     
     # Calls an infinte loop for a screen, and enables close/minimise button on pygame screen
     while True:
@@ -18,6 +21,10 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+    
+    #Limits FPS
+    dt = clock.tick(60) / 1000
+    
     #Start up text
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
