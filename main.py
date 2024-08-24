f91271089c9162662ef9ed10e73bc514f98bf495
Riatasthan <153391887+Riatasthan@users.noreply.github.com>
2024-08-24 08:01:33 +0100

    Working screen

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 # Importing module
 import pygame
 
-# Initalising the module
-pygame.init()
-
 # Imporitng constants from constants file
 from constants import *
 
-# Setting screen size 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-# Calls an infinte loop for a screen
-while True:
-    pygame.Surface.fill(screen, (0, 255, 0))
-    
-
 # Main loop
 def main():
+    # Initalising the module
+    pygame.init()
+    # Setting screen size 
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    # Calls an infinte loop for a screen, and enables close/minimise button on pygame screen
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+    #Start up text
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
 
 
 if __name__ == "__main__":
